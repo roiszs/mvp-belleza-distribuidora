d0b078f7156ba0de02ffa8d1cf774e4f875bb60f
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2026-04-02 19:15:39 -0500

    Imagenes de categorias

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -5,24 +5,24 @@ export const categories: Category[] = [
     id: 'geles',
     name: 'Gel semipermanente',
     description: 'Colecciones Le Mussa, tonos clásicos y gamas de alta rotación para cabina, salón y reventa.',
-    image: '/images/categories/geles.jpg',
+    image: '/images/categories/gel-semipermanente.jpeg',
   },
   {
     id: 'acrilico',
     name: 'Acrílico y preparación',
     description: 'Polvo acrílico, bases, limas y esenciales para preparación y servicio profesional.',
-    image: '/images/categories/acrilico.jpg',
+    image: '/images/categories/acrilico-preparacion.jpeg',
   },
   {
     id: 'tips',
     name: 'Tips y construcción',
     description: 'Tips Soft Gel, formatos coffin y materiales para armado de sets con mejor presentación.',
-    image: '/images/categories/tips.jpg',
+    image: '/images/categories/tips-construccion.jpeg',
   },
   {
     id: 'decoracion',
     name: 'Decoración y herramientas',
     description: 'Efectos espejo, flake, cristales, katanas, lámparas y accesorios para mesa de trabajo.',
-    image: '/images/categories/decoracion.jpg',
+    image: '/images/categories/decoracion-herramientas.jpeg',
   },
 ]
